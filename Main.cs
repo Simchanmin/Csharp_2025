117787c45a69e3adbe099b365ecd0599862aafcd
Simchanmin <simcm20003@gmail.com>
2025-10-14 15:37:38 +0900

    asd

---
+++ Main.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Text;
+
+class MainProgram
+{
+    static void MainMethod(string[] args)
+    {
+        string term = "1";
+        for (int i = 1; i <= 20; i++)
+        {
+            Console.WriteLine($"{i}번째: {term}");
+            term = Next(term);
+        }
+        Console.WriteLine("계속하려면 아무 키나 누르십시오 . . .");
+        Console.ReadKey(true);
+    }
+
+    static string Next(string s)
+    {
+        var sb = new StringBuilder();
+        int count = 1;
+        for (int i = 1; i <= s.Length; i++)
+        {
+            if (i < s.Length && s[i] == s[i - 1]) count++;
+            else { sb.Append(count); sb.Append(s[i - 1]); count = 1; }
+        }
+        return sb.ToString();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,29 +1,38 @@
 ﻿using System;
-using System.Text;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        string term = "1";
-        for (int i = 1; i <= 20; i++)
-        {
-            Console.WriteLine($"{i}번째: {term}");
-            term = Next(term);
-        }
+        Console.WriteLine("첫 번째 배열의 숫자들을 공백으로 구분하여 입력하세요:");
+        string? input1 = Console.ReadLine();
+        Console.WriteLine("두 번째 배열의 숫자들을 공백으로 구분하여 입력하세요:");
+        string? input2 = Console.ReadLine();
+
+        int[] array1 = input1.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                             .Select(int.Parse)
+                             .ToArray();
+
+        int[] array2 = input2.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                             .Select(int.Parse)
+                             .ToArray();
+
+        Console.WriteLine("\n첫 번째 배열:");
+        Console.WriteLine("{ " + string.Join(" ", array1) + " }");
+
+        Console.WriteLine("두 번째 배열:");
+        Console.WriteLine("{ " + string.Join(" ", array2) + " }");
+
+
+        int[] mergedArray = array1.Concat(array2).ToArray();
+        Array.Sort(mergedArray);
+
+        Console.WriteLine("\n합쳐서 정렬한 결과:");
+        Console.WriteLine("{ " + string.Join(" ", mergedArray) + " }");
+
+        Console.WriteLine();
         Console.WriteLine("계속하려면 아무 키나 누르십시오 . . .");
         Console.ReadKey(true);
-    }
-
-    static string Next(string s)
-    {
-        var sb = new StringBuilder();
-        int count = 1;
-        for (int i = 1; i <= s.Length; i++)
-        {
-            if (i < s.Length && s[i] == s[i - 1]) count++;
-            else { sb.Append(count); sb.Append(s[i - 1]); count = 1; }
-        }
-        return sb.ToString();
     }
 }
